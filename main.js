a455816607450770ce2131d6041398993c748fe2
IsaiahThao <chumengt@gmail.com>
2026-03-23 15:41:36 -0500

    Create game logic

--- main.js
+++ main.js
@@ -1,25 +1,62 @@
 const GameBoard = (function(){
     let gameBoard = ["","","","","","","","",""];
 
-    function updateBoard(index){
-        gameBoard[index] = 'X';
-        console.log(gameBoard);
+    function updateBoard(index, symbol){
+        if (gameBoard[index] == ''){
+            gameBoard[index] = symbol;
+            console.log(gameBoard);
+            return true
+        }
+        else{
+            console.log('already chosen');
+            return false
+            
+        }
     }
 
-    return {updateBoard};
+    function returnBoard(){
+        return gameBoard;
+    }
+
+    return {updateBoard, returnBoard};
 }())
 
 function checkWinner(){
 
 }
 
+function createPlayer(name, symbol) {
+    return {
+        name: name,
+        symbol: symbol
+    };
+}
+
+function checkTurn(currentPlayer){
+    if (currentPlayer == player1){
+        console.log(currentPlayer)
+        return currentPlayer = player2
+    }
+    else{
+        console.log(currentPlayer)
+        return currentPlayer = player1
+    }
+}
+
+
+const player1 = createPlayer("Player 1", "X");
+const player2 = createPlayer("Player 2", "O");
+
 const cells = document.querySelectorAll('.cell');
+let currentPlayer = player1
+
 cells.forEach(cell => {
     cell.addEventListener('click', (e) => {
         e.target.style.backgroundColor = 'red';
-        console.log(e.target.id);
-        GameBoard.updateBoard(Number(e.target.id));
+        GameBoard.updateBoard(Number(e.target.id), currentPlayer.symbol);
+        currentPlayer = checkTurn(currentPlayer)
         checkWinner();
     });
 });
+
 
